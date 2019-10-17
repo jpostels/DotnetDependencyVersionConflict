d35e1bf8c5286a323f33d0dd1bb4db6c4667cd28
Jonas Postels <dev@j-postels.de>
2019-10-17 19:21:44 +0200

    Unpack nugets and manually load dependencies

--- ExampleDependencyApp/Program.cs
+++ ExampleDependencyApp/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
+using System.Reflection;
 using LibraryA;
 using LibraryB;
 
@@ -9,6 +11,8 @@ namespace ExampleDependencyApp
     {
         public static void Main()
         {
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+
             Console.WriteLine("JSON A: " + ClassA.GetJsonAssemblyInfos());
 
             Console.WriteLine(Environment.NewLine);
@@ -18,6 +22,27 @@ namespace ExampleDependencyApp
 
             Debug.Assert(ClassA.GetJsonAssemblyVersion().Major != ClassB.GetJsonAssemblyVersion().Major,
                 "Expected: A " + ClassA.GetJsonAssemblyVersion() + " !=  B " + ClassB.GetJsonAssemblyVersion());
+        }
+
+        private static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            if (args.Name.Equals("Newtonsoft.Json, Version=11.0.0.0, Culture=neutral, PublicKeyToken=30ad4fe6b2a6aeed"))
+            {
+                return Assembly.LoadFile(
+                    Path.Combine(
+                        AppContext.BaseDirectory,
+                        "json11\\newtonsoft.json.dll"));
+            }
+
+            if (args.Name.Equals("Newtonsoft.Json, Version=12.0.0.0, Culture=neutral, PublicKeyToken=30ad4fe6b2a6aeed"))
+            {
+                return Assembly.LoadFile(
+                    Path.Combine(
+                        AppContext.BaseDirectory,
+                        "json12\\newtonsoft.json.dll"));
+            }
+
+            return default;
         }
     }
 }
