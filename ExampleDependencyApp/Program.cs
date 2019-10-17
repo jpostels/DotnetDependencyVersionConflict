a1e50a6e2e4743bc6c2af7632ad66e3decfe0624
Jonas Postels <dev@j-postels.de>
2019-10-17 18:22:27 +0200

    Add project files.

---
+++ ExampleDependencyApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExampleDependencyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
